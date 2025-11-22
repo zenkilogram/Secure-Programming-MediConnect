e761c1815795b0bb4b4a79a2703161ec24c479c0
Revatha <afelia.arelian10@gmail.com>
2025-11-23 00:09:03 +0700

    Update AuditLog.php

--- app/Models/AuditLog.php
+++ app/Models/AuditLog.php
@@ -2,9 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class AuditLog extends Model
 {
-    //
+    use HasFactory;
+    
+    // Pastikan nama tabel sesuai
+    protected $table = 'audit_logs';
+
+    // Izinkan input data
+    protected $guarded = []; 
 }
