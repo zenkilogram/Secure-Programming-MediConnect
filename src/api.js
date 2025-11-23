842c2cb61bb0a73df952bfb95f6997c99d4736f4
Revatha <afelia.arelian10@gmail.com>
2025-11-23 16:00:57 +0700

    Update

--- src/api.js
+++ src/api.js
@@ -1,11 +1,15 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000",
-  withCredentials: true,
+  baseURL: "http://localhost:8000/api/v1",
 });
 
-export const getCSRFToken = () =>
-  api.get("/sanctum/csrf-cookie");
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
 
 export default api;
--- src/pages/AuditLogPage.jsx
+++ src/pages/AuditLogPage.jsx
@@ -111,3 +111,5 @@ const AuditLogPage = () => {
         </AdminLayout>
     );
 };
+
+export default AuditLogPage;
